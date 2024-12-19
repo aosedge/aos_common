e265cb26913b42c2e4826fc82b10b36c6c8ce1d5
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-12-19 15:59:23 +0200

    [aostypes] Revert back changing marshal interface for time and duration
    These changes causes wrong marshaling, suppress warning instead of changing
    interface.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- aostypes/aostypes.go
+++ aostypes/aostypes.go
@@ -66,12 +66,12 @@ const (
  **********************************************************************************************************************/
 
 // Duration represents duration in format "00:00:00".
-type Duration struct {
+type Duration struct { //nolint:recvcheck
 	time.Duration
 }
 
 // Time represents time in format "00:00:00".
-type Time struct {
+type Time struct { //nolint:recvcheck
 	time.Time
 }
 
@@ -262,7 +262,7 @@ type NodeMonitoring struct {
  **********************************************************************************************************************/
 
 // MarshalJSON marshals JSON Time type.
-func (t *Time) MarshalJSON() (b []byte, err error) {
+func (t Time) MarshalJSON() (b []byte, err error) {
 	if b, err = json.Marshal(t.Format("15:04:05")); err != nil {
 		return nil, aoserrors.Wrap(err)
 	}
@@ -319,7 +319,7 @@ func (t *Time) UnmarshalJSON(b []byte) (err error) {
 }
 
 // MarshalJSON marshals JSON Duration type.
-func (d *Duration) MarshalJSON() (b []byte, err error) {
+func (d Duration) MarshalJSON() (b []byte, err error) {
 	if b, err = json.Marshal(d.Duration.String()); err != nil {
 		return nil, aoserrors.Wrap(err)
 	}
